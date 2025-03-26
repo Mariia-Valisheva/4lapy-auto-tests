0929442c912cb12fbfd8e7eec91f68d98f1d43c2
Maria Valisheva <dolnish9@gmail.com>
2025-03-26 22:04:04 +0300

    diploma

--- src/test/java/config/SelenoidConfig.java
+++ src/test/java/config/SelenoidConfig.java
@@ -4,8 +4,7 @@ import org.aeonbits.owner.Config;
 
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
-        "classpath:credentials.properties",
-        "system:env"
+        "classpath:credentials.properties"
 })
 
 public interface SelenoidConfig extends Config {
